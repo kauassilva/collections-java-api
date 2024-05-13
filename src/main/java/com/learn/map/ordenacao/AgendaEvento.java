2f915602f3002f11ecc4342a3dafc235cb954dee
kauassilva <kauassilva1@gmail.com>
2024-05-13 16:32:58 -0300

    Add Agenda de Eventos activity
    Signed-off-by: kauassilva <kauassilva1@gmail.com>

---
+++ src/main/java/com/learn/map/ordenacao/AgendaEvento.java
@@ -0,0 +1,62 @@
+package com.learn.map.ordenacao;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class AgendaEvento {
+
+    private Map<LocalDate, Evento> eventosMap;
+
+    public AgendaEvento() {
+        eventosMap = new HashMap<>();
+    }
+
+    public void adicionarEvento(LocalDate data, String nome, String atracao) {
+        eventosMap.put(data, new Evento(nome, atracao));
+    }
+
+    public void exibirAgenda() {
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        System.out.println(eventosTreeMap);
+    }
+
+    public void obterProximoEvento() {
+        LocalDate dataAtual = LocalDate.now();
+        LocalDate proximaData = null;
+        Evento proximoEvento = null;
+
+        for (Map.Entry<LocalDate, Evento> entry : eventosMap.entrySet()) {
+            LocalDate dataEvento = entry.getKey();
+
+            if (dataEvento.isEqual(dataAtual) || dataEvento.isAfter(dataAtual)) {
+                proximaData = dataEvento;
+                proximoEvento = entry.getValue();
+                break;
+            }
+        }
+
+        if (proximoEvento != null) {
+            System.out.println("O próximo evento: " + proximoEvento.getNome() + " acontecerá na data " + proximaData);
+        } else {
+            System.out.println("Não há eventos futuro na agenda");
+        }
+    }
+
+    public static void main(String[] args) {
+        AgendaEvento agendaEventos = new AgendaEvento();
+
+        agendaEventos.adicionarEvento(LocalDate.of(2022, Month.JULY, 15), "Conferência de Tecnologia", "Palestrante renomado");
+        agendaEventos.adicionarEvento(LocalDate.of(2022, 7, 9), "Workshop de Programação", "Aula prática de desenvolvimento");
+        agendaEventos.adicionarEvento(LocalDate.of(2000, 1, 10), "Lançamento de Software", "Demonstração da nova versão");
+        agendaEventos.adicionarEvento(LocalDate.of(2023, Month.AUGUST, 28), "Hackathon de Inovação", "Competição de soluções criativas");
+        agendaEventos.adicionarEvento(LocalDate.of(2024, 9, 20), "Seminário de Inteligência Artificial", "Discussão sobre IA avançada");
+
+        agendaEventos.exibirAgenda();
+
+        agendaEventos.obterProximoEvento();
+    }
+
+}
